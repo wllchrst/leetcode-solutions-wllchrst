85eb04b04e6e5236a4a03191f5faa74693295b7a
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-07 13:31:42 +0700

    feat: Is Anagram

--- is-anagram.cpp
+++ is-anagram.cpp
@@ -18,8 +18,32 @@ public:
         const int sLength = s.length();
         for (int i = 0; i < sLength; i++)
         {
-            int value = firstMap.at(s[i]);
+            auto firstFinder = firstMap.find(s[i]);
+            auto secondFinder = secondMap.find(t[i]);
+
+            if (firstFinder == firstMap.end())
+            {
+                firstMap.insert({s[i], 1});
+            }
+            else
+                firstMap.insert({s[i], firstFinder->second + 1});
+
+            if (secondFinder == secondMap.end())
+                secondMap.insert({t[i], 1});
+            else
+                secondMap.insert({t[i], secondFinder->second + 1});
         }
+
+        for (const auto &pair : firstMap)
+        {
+            auto finder = secondMap.find(pair.first);
+            if (finder == secondMap.end())
+                return false;
+            else if (finder->second != pair.second)
+                return false;
+        }
+
+        return true;
     }
 };
 
